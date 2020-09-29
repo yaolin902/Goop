d1b013d8f5063381139970feb5f7c958e60c95e6
uygwfd254 <uygwfd254@gmail.com>
2020-09-29 10:35:18 -0700

    added player move left and right and jump

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float jump_force = 7f;
+    [SerializeField]
+    private float speed = 10f;
+    [SerializeField]
+    private LayerMask layer;
+
+    private Rigidbody2D player;
+    private BoxCollider2D collider;
+    private void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
+    }
+    private void FixedUpdate()
+    {
+        player.velocity = new Vector2(speed * Input.GetAxis("Horizontal"), player.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && is_grounded())
+            player.velocity = new Vector2(player.velocity.x, jump_force);
+    }
+
+    private bool is_grounded() {
+        RaycastHit2D hit = Physics2D.BoxCast(collider.bounds.center, collider.bounds.size, 0f, Vector2.down, .5f, layer);
+        return hit.collider != null;
+    }
+}
