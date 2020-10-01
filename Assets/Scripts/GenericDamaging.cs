74d055c900ad822ecc936dc9736648d740d33601
PersonVonIncognito <68795964+PersonVonIncognito@users.noreply.github.com>
2020-10-01 16:16:29 -0700

    Basic Damage and Health Scripts

---
+++ Assets/Scripts/GenericDamaging.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericDamaging : MonoBehaviour
+{
+	[SerializeField] internal int damage = 0;
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		collision.gameObject.SendMessage("takeDamage", damage);
+	}
+}
---
+++ Assets/Scripts/GenericHealth.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericHealth : MonoBehaviour
+{
+	[SerializeField] int health = 0;
+	
+	void takeDamage(int toDamage)
+	{
+		health -= toDamage;
+		if (health <= 0) Destroy(gameObject);
+	}
+}
