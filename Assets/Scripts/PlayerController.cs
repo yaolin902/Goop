03e3ef35d6e249423116579bfc06e23f02bf93bc
uygwfd254 <uygwfd254@gmail.com>
2020-10-10 21:04:59 -0700

    Revert "merged Shaun and Yao's progress"
    This reverts commit 05988b844745887a2c80f559bde2ac45a98e1b76, reversing
    changes made to 50e150e5f3227747181a6aadb8e5246bab6517f0.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,65 +9,25 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float speed = 10f;
     [SerializeField]
-    private LayerMask ground_layer;
-    [SerializeField]
-    private LayerMask platform_layer;
+    private LayerMask layer;
 
     private Rigidbody2D player;
-    private GameObject projectile_player;
-    private BoxCollider2D ground_collider;
-
-    private bool is_facing_right = true;
+    private BoxCollider2D collider;
     private void Start()
     {
         player = GetComponent<Rigidbody2D>();
-        ground_collider = GetComponent<BoxCollider2D>();
-        projectile_player = GameObject.FindWithTag("Projectile Player");
+        collider = GetComponent<BoxCollider2D>();
     }
     private void FixedUpdate()
     {
-        // player movement
-        float move_x = Input.GetAxis("Horizontal");
-        player.velocity = new Vector2(speed * move_x, player.velocity.y);
+        player.velocity = new Vector2(speed * Input.GetAxis("Horizontal"), player.velocity.y);
 
-        // jump
-        if (Input.GetKeyDown(KeyCode.Space) && (is_grounded() || is_platformed()))
+        if (Input.GetKeyDown(KeyCode.Space) && is_grounded())
             player.velocity = new Vector2(player.velocity.x, jump_force);
-
-        // fall through platform
-        if (Input.GetKeyDown(KeyCode.DownArrow) && is_platformed())
-            StartCoroutine(fall_down());
-
-        // player direction
-        if ((move_x < 0f && is_facing_right) ||
-            (move_x > 0f && !is_facing_right))
-            flip();
-
     }
 
-    // ground checks
     private bool is_grounded() {
-        RaycastHit2D hit = Physics2D.BoxCast(ground_collider.bounds.center, ground_collider.bounds.size, 0f, Vector2.down, .5f, ground_layer);
+        RaycastHit2D hit = Physics2D.BoxCast(collider.bounds.center, collider.bounds.size, 0f, Vector2.down, .5f, layer);
         return hit.collider != null;
     }
-
-    private bool is_platformed() {
-        RaycastHit2D hit = Physics2D.BoxCast(ground_collider.bounds.center, ground_collider.bounds.size, 0f, Vector2.down, .5f, platform_layer);
-        return hit.collider != null;
-    }
-
-    private IEnumerator fall_down() {
-        ground_collider.enabled = false;
-        yield return new WaitForSeconds(0.6f);
-        ground_collider.enabled = true;
-    }
-
-    private void flip() {
-        is_facing_right = !is_facing_right;
-        transform.localScale = new Vector2(-1 * transform.localScale.x, transform.localScale.y);
-
-        // also flip projectile player direction
-        projectile_player.SendMessage("flip");
-    }
-    
 }
