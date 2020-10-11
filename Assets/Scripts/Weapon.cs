806b18798fd44f0e5567166e94822b43c05c4e3a
uygwfd254 <uygwfd254@gmail.com>
2020-10-10 23:15:57 -0700

    manually merged all branches

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    // FirePoint
+    public Transform firePoint;
+    public GameObject gaapPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+        
+    }
+
+    // Shoot
+    void Shoot()
+    {
+        // spawn
+        Instantiate(gaapPrefab, firePoint.position, firePoint.rotation);
+    }
+}
