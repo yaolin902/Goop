97ebbf11788936edb3c7373b7fbbe3150c95b0bf
uygwfd254 <uygwfd254@gmail.com>
2020-11-13 14:39:08 -0800

    added cooldown for shooting, improve bullet collision, tested Shaun's wizard (good)

--- Assets/Scripts/Enemy/WizardShoot.cs
+++ Assets/Scripts/Enemy/WizardShoot.cs
@@ -6,12 +6,13 @@ public class WizardShoot : MonoBehaviour
 {
 	// References what to shoot and at who
 	[SerializeField] internal GameObject projectilePrefab;
-	[SerializeField] internal GameObject firePoint;
+	[SerializeField] internal Transform firePoint;
+	[SerializeField] float bullet_speed;
 	internal GameObject player;
 
 	// Uses how many seconds in reload rate to determine how fast
 	// the character shoots.
-	internal float reloadRate = 5;
+	internal float reloadRate = 5f;
 	internal float reloadTime;
 	
     // Start is called before the first frame update
@@ -36,7 +37,15 @@ public class WizardShoot : MonoBehaviour
 	void Shoot()
 	{
 		// Looks at player before creating bullet and firing at them from the firePointS
-		firePoint.transform.LookAt(player.transform);
-		Instantiate(projectilePrefab, firePoint.transform.position, firePoint.transform.rotation);
+		//firePoint.transform.LookAt(player.transform);
+		GameObject bullet = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+		Rigidbody2D bullet_rb = bullet.GetComponent<Rigidbody2D>();
+
+		// wizard shoots left or right
+		float x_diff = this.transform.position.x - player.transform.position.x;
+		if (x_diff >= 0)
+			bullet_rb.velocity = new Vector2(-bullet_speed, bullet_rb.velocity.y);
+		else
+			bullet_rb.velocity = new Vector2(bullet_speed, bullet_rb.velocity.y);
 	}
 }
---
+++ Assets/Scripts/Player/BulletCollision.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCollision : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.layer == 8 || collision.gameObject.layer == 9)
+            Destroy(this.gameObject);
+    }
+}
